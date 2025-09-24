a6270d4c736a05e039b80bc4bb08abbfad61b2d1
Raffaele Pumpo <raffaele.pumpo@iit.it>
2025-09-24 09:20:46 +0200

    Add SET CAN message

--- boards/amcbldc/system/dictionaries/enums/CAN/MCControlModes.m
+++ boards/amcbldc/system/dictionaries/enums/CAN/MCControlModes.m
@@ -4,13 +4,13 @@
 classdef MCControlModes < Simulink.IntEnumType
     enumeration
         Idle(0x00)
+        Position(0x01)
         OpenLoop(0x50)
         SpeedVoltage(0x0A)
         SpeedCurrent(0x0B)
         Current(0x06)
         NotConfigured(0xB0)
         HWFault(0xA0)
-        Position(0x07)
     end
     methods (Static = true)
         function retVal = addClassNameToEnumNames()
